4b6fca246bc11990f6b968a4dff98d9fdfc639cf
Hannes Hasenauer <hannes.hasenauer@student.tugraz.at>
2011-10-16 15:00:11 +0200

    Just my Testfiles

--- test.php
+++ test.php
@@ -1,11 +1,20 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-$testing = "Hallo";
-echo "Sag $testing";
-
-?>
-Wie gehts?
+<html>  
+    
+    <?php
+        $testing = "Hallo";
+        echo "Sag $testing";
+    ?>
+    
+    <form action="test.php" method="get">
+        <input type="text" name="thebox" value=""/>
+        <input type="submit" value="Go..."/>
+    </form>
+    
+    
+    Dein Name ist: 
+    <?php
+        $name = $_GET["thebox"];
+        echo "$name";
+    ?>
+    
+</html>
