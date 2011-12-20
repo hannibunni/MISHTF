8892ad8f42a8435e6587ff5a8a2fd1205681abfc
Daniel Fritzsch <d.fritzsch@student.tugraz.at>
2011-12-20 14:39:24 +0100

    basic refactoring

--- MISHTF.sql
+++ MISHTF.sql
@@ -1,14 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 20. Dezember 2011 um 13:19
--- Server Version: 5.1.44
--- PHP-Version: 5.3.1
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,7 +8,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Datenbank: `BURGER_HOUSE`
 --
-CREATE DATABASE `BURGER_HOUSE` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE `BURGER_HOUSE` DEFAULT CHARACTER SET latin1;
 USE `BURGER_HOUSE`;
 
 -- --------------------------------------------------------
@@ -32,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `Burger` (
   `BName` varchar(15) NOT NULL,
   `BPrice` int(3) unsigned NOT NULL,
   PRIMARY KEY (`BID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+);
 
 --
 -- Daten für Tabelle `Burger`
@@ -48,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `Burger` (
 CREATE TABLE IF NOT EXISTS `Menu` (
   `MID` int(5) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`MID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+);
 
 --
 -- Daten für Tabelle `Menu`
@@ -67,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `Serve` (
   `TID` int(5) unsigned NOT NULL,
   `SDate` date NOT NULL,
   PRIMARY KEY (`SID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+);
 
 --
 -- Daten für Tabelle `Serve`
@@ -86,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `SideDishes` (
   `SPrice` int(2) unsigned NOT NULL,
   `SGlutenFree` tinyint(1) NOT NULL,
   PRIMARY KEY (`SID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+);
 
 --
 -- Daten für Tabelle `SideDishes`
@@ -104,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `Table` (
   `TSmoker` tinyint(1) NOT NULL,
   `TSeats` int(2) unsigned NOT NULL,
   PRIMARY KEY (`TID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+);
 
 --
 -- Daten für Tabelle `Table`
@@ -125,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `Waiter` (
   `WAge` int(3) NOT NULL,
   `WGender` varchar(10) NOT NULL,
   PRIMARY KEY (`WID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+);
 
 --
 -- Daten für Tabelle `Waiter`
