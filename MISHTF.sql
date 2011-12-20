8c771a9993c6b2933716504d39c7c64dd0bf4f9b
Daniel Fritzsch <d.fritzsch@student.tugraz.at>
2011-12-20 14:20:22 +0100

    modified dump

--- MISHTF.sql
+++ MISHTF.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 20. Dezember 2011 um 13:02
+-- Erstellungszeit: 20. Dezember 2011 um 13:19
 -- Server Version: 5.1.44
 -- PHP-Version: 5.3.1
 
@@ -18,6 +18,98 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Datenbank: `BURGER_HOUSE`
 --
+CREATE DATABASE `BURGER_HOUSE` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `BURGER_HOUSE`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `Burger`
+--
+
+CREATE TABLE IF NOT EXISTS `Burger` (
+  `BID` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `BName` varchar(15) NOT NULL,
+  `BPrice` int(3) unsigned NOT NULL,
+  PRIMARY KEY (`BID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Daten für Tabelle `Burger`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `Menu`
+--
+
+CREATE TABLE IF NOT EXISTS `Menu` (
+  `MID` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`MID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Daten für Tabelle `Menu`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `Serve`
+--
+
+CREATE TABLE IF NOT EXISTS `Serve` (
+  `SID` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `WID` int(5) unsigned NOT NULL,
+  `TID` int(5) unsigned NOT NULL,
+  `SDate` date NOT NULL,
+  PRIMARY KEY (`SID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Daten für Tabelle `Serve`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `SideDishes`
+--
+
+CREATE TABLE IF NOT EXISTS `SideDishes` (
+  `SID` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `SName` varchar(15) NOT NULL,
+  `SPrice` int(2) unsigned NOT NULL,
+  `SGlutenFree` tinyint(1) NOT NULL,
+  PRIMARY KEY (`SID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Daten für Tabelle `SideDishes`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `Table`
+--
+
+CREATE TABLE IF NOT EXISTS `Table` (
+  `TID` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `TSmoker` tinyint(1) NOT NULL,
+  `TSeats` int(2) unsigned NOT NULL,
+  PRIMARY KEY (`TID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Daten für Tabelle `Table`
+--
+
 
 -- --------------------------------------------------------
 
@@ -25,7 +117,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Tabellenstruktur für Tabelle `Waiter`
 --
 
-DROP TABLE IF EXISTS `Waiter`;
 CREATE TABLE IF NOT EXISTS `Waiter` (
   `WID` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `WFirstName` varchar(30) NOT NULL,
